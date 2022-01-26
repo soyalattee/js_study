41f5a8714b58bda612823ed489ebb58df593f246
박소연 <soyalatte@neowiz.com>
2022-01-27 03:37:47 +0900

    first commit fist week

---
+++ week01/main.js
@@ -0,0 +1,23 @@
+// use strict ( add in ES5)
+"use strict";
+console.log("hello world");
+
+//variable 변수 :변경될 수 있는 값
+// let ( added in ES6)
+let a;
+a = 6;
+{
+  let letname = "soya";
+  const consName = "SOYA";
+  console.log(letname);
+  console.log(consName);
+}
+// console.log(letname);
+
+// console.log("읽어????", consname);
+console.log(a);
+
+console.log(age);
+age = 4;
+console.log(age);
+var age;
